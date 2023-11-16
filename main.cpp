928c32fb958056e1a095d570e8418543dad098c6
9weights <68071656+9weights@users.noreply.github.com>
2023-11-16 23:52:26 +0000

    update gitignore

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <vector>
+
+// 1. create board
+//
+//2. create paddles
+//
+//3. create ball
+
+
+class Board{
+private:
+    int tem{};
+
+public:
+    Board(int nx, int ny)
+        : m_nx{nx}
+        , m_ny{ny}
+    {
+        std::vector<> m_board
+    }
+
+
+
+
+}
+
 int main(){
-    int temp{};
+    
     return 0;
 }
